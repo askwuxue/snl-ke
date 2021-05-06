d600289300cc7ebb87d024385bd6fc974af3d3ae
askwuxue <972523419@qq.com>
2021-05-06 23:28:54 +0800

    usehydrate and server SSR

--- web/src/index.js
+++ web/src/index.js
@@ -4,14 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// TODO 使用hydrate 对服务器返回的数据进行处理，如果root下面有内容，忽略首次渲染。只有当页面内容发生变化的时候才调用render。首次是SSR渲染。
+ReactDOM.hydrate(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+  );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
