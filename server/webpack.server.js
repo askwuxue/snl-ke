00916579f45c30a6b1221e698da02e5ebd651a88
askwuxue <972523419@qq.com>
2021-05-07 00:28:42 +0800

    js project upgrade ts

---
+++ server/webpack.server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+module.exports = {
+    entry: './src/server.ts',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'server.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader'
+            }
+        ]
+    }
+}
