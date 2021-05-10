ce87dd13acc646c1e79e6a33dae3b66ba01a95bc
askwuxue <972523419@qq.com>
2021-05-10 21:29:23 +0800

    file spliting and use configuration files

--- server/src/server.tsx
+++ server/src/server.tsx
@@ -1,27 +1,15 @@
-import Koa from 'koa';
-import Router from '@koa/router';
-// TODO mysql不支持异步操作，使用的是回调函数的方式。所以使用mysql/promise
-import mysql from  'mysql2/promise';
 
-const app = new Koa();
-const port = 8080;
+import Router from '@koa/router';
+import database from '../libs/database';
+import app from '../libs/server';
 
 app.use( async ctx => {
-    const conn = await mysql.createConnection({
-        host: 'localhost',
-        port: 3306,
-        user: 'root',
-        password: 'root',
-        database: 'snl'
-    });
     // TODO mysql查询到的数组中，数据是rows, files是连接相关的信息。
-    const result = await conn.query('SELECT * from banner_table;');
+    const result = await database.query('SELECT * from banner_table;');
     const [rows, fields] = result;
-    console.log('rows: ', rows);
+    // console.log('rows: ', rows); 
     ctx.body = rows;
 })
 
-app.listen(port, () => {
-    console.log(`server started at port ${port}`);
-})
+
 
