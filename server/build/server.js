663bd8a5e51bf933bf0989f752d1f2807c44bc78
askwuxue <972523419@qq.com>
2021-05-10 22:49:52 +0800

    create type declaration for mysql and redis

--- server/build/server.js
+++ server/build/server.js
@@ -26,7 +26,7 @@ return /******/ (() => { // webpackBootstrap
   \***********************/
 /***/ ((__unused_webpack_module, exports) => {
 
-eval("\r\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\r\nexports.db = void 0;\r\nexports.db = {\r\n    host: 'localhost',\r\n    port: 3306,\r\n    user: 'root',\r\n    password: 'root',\r\n    database: 'snl'\r\n};\r\n\n\n//# sourceURL=webpack://server/./configs/db.ts?");
+eval("\r\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\r\nexports.default = {\r\n    host: 'localhost',\r\n    port: 3306,\r\n    user: 'root',\r\n    password: 'root',\r\n    database: 'snl'\r\n};\r\n\n\n//# sourceURL=webpack://server/./configs/db.ts?");
 
 /***/ }),
 
@@ -46,7 +46,7 @@ eval("\r\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\r\n
   \**************************/
 /***/ (function(__unused_webpack_module, exports, __webpack_require__) {
 
-eval("\r\nvar __importDefault = (this && this.__importDefault) || function (mod) {\r\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\r\n};\r\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\r\n// TODO mysql不支持异步操作，使用的是回调函数的方式。所以使用mysql/promise\r\nconst promise_1 = __importDefault(__webpack_require__(/*! mysql2/promise */ \"mysql2/promise\"));\r\n// 导入DB配置\r\nconst db_1 = __webpack_require__(/*! ../configs/db */ \"./configs/db.ts\");\r\n// TODO 创建连接池\r\nexports.default = promise_1.default.createPool(db_1.db);\r\n\n\n//# sourceURL=webpack://server/./libs/database.ts?");
+eval("\r\nvar __importDefault = (this && this.__importDefault) || function (mod) {\r\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\r\n};\r\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\r\n// TODO mysql不支持异步操作，使用的是回调函数的方式。所以使用mysql/promise\r\nconst promise_1 = __importDefault(__webpack_require__(/*! mysql2/promise */ \"mysql2/promise\"));\r\n// 导入DB配置\r\nconst db_1 = __importDefault(__webpack_require__(/*! ../configs/db */ \"./configs/db.ts\"));\r\n// TODO 创建连接池\r\nexports.default = promise_1.default.createPool(db_1.default);\r\n\n\n//# sourceURL=webpack://server/./libs/database.ts?");
 
 /***/ }),
 
@@ -60,13 +60,23 @@ eval("\r\nvar __importDefault = (this && this.__importDefault) || function (mod)
 
 /***/ }),
 
+/***/ "./models/banner.ts":
+/*!**************************!*\
+  !*** ./models/banner.ts ***!
+  \**************************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+eval("\r\nvar __importDefault = (this && this.__importDefault) || function (mod) {\r\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\r\n};\r\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\r\nexports.getAllBanners = void 0;\r\nconst database_1 = __importDefault(__webpack_require__(/*! ../libs/database */ \"./libs/database.ts\"));\r\n// 返回的是Promise类型的BannerData数组\r\nasync function getAllBanners() {\r\n    // TODO mysql查询到的数组中，数据是rows, files是连接相关的信息。\r\n    const result = await database_1.default.query('SELECT * from banner_table;');\r\n    // TODO 将result 强制转成BannerData类型的数组\r\n    const rows = result[0];\r\n    return rows;\r\n}\r\nexports.getAllBanners = getAllBanners;\r\n\n\n//# sourceURL=webpack://server/./models/banner.ts?");
+
+/***/ }),
+
 /***/ "./src/server.tsx":
 /*!************************!*\
   !*** ./src/server.tsx ***!
   \************************/
 /***/ (function(__unused_webpack_module, exports, __webpack_require__) {
 
-eval("\r\nvar __importDefault = (this && this.__importDefault) || function (mod) {\r\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\r\n};\r\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\r\nconst database_1 = __importDefault(__webpack_require__(/*! ../libs/database */ \"./libs/database.ts\"));\r\nconst server_1 = __importDefault(__webpack_require__(/*! ../libs/server */ \"./libs/server.ts\"));\r\nserver_1.default.use(async (ctx) => {\r\n    // TODO mysql查询到的数组中，数据是rows, files是连接相关的信息。\r\n    const result = await database_1.default.query('SELECT * from banner_table;');\r\n    const [rows, fields] = result;\r\n    // console.log('rows: ', rows); \r\n    ctx.body = rows;\r\n});\r\n\n\n//# sourceURL=webpack://server/./src/server.tsx?");
+eval("\r\nvar __importDefault = (this && this.__importDefault) || function (mod) {\r\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\r\n};\r\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\r\nconst server_1 = __importDefault(__webpack_require__(/*! ../libs/server */ \"./libs/server.ts\"));\r\nconst banner_1 = __webpack_require__(/*! ../models/banner */ \"./models/banner.ts\");\r\nserver_1.default.use(async (ctx) => {\r\n    // 返回的是一个Promise对象，Promise的结果是BannerData类型的数组\r\n    ctx.body = await banner_1.getAllBanners();\r\n});\r\n// 使用Redis\r\n// app.use(async ctx => {\r\n//     ctx.body = await redis.get('a');\r\n// })\r\n\n\n//# sourceURL=webpack://server/./src/server.tsx?");
 
 /***/ }),
 
--- server/src/server.tsx
+++ server/src/server.tsx
@@ -1,20 +1,18 @@
 
 import Router from '@koa/router';
-import database from '../libs/database';
 import app from '../libs/server';
 import * as redis from '../libs/redis';
+import { getAllBanners } from '../models/banner';
 
-// app.use( async ctx => {
-//     // TODO mysql查询到的数组中，数据是rows, files是连接相关的信息。
-//     // const result = await database.query('SELECT * from banner_table;');
-//     // const [rows, fields] = result;
-//     // console.log('rows: ', rows); 
-//     // ctx.body = rows;
-// })
-
-app.use(async ctx => {
-    ctx.body = await redis.get('a');
+app.use( async ctx => {
+    // 返回的是一个Promise对象，Promise的结果是BannerData类型的数组
+    ctx.body = await getAllBanners();
 })
+
+// 使用Redis
+// app.use(async ctx => {
+//     ctx.body = await redis.get('a');
+// })
 
 
 
