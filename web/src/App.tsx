cd1da2a397cc9444e176754c1752cc886e6823d9
askwuxue <972523419@qq.com>
2021-05-13 23:01:16 +0800

    delete annotations

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-// import logo from './logo.svg';
 import axios from 'axios';
 import './App.css';
 
 function App() {
   axios({
-    method: 'get',
-    url: 'http://localhost:3000/api/course',
-  })
+        method: 'get',
+        url: 'http://localhost:8000/api/course',
+    })
     .then(data => {
       console.log(data);
     })
