5455aaa140e6db5b2dbf20fe928af5387797d601
askwuxue <972523419@qq.com>
2021-05-15 00:04:28 +0800

    dispose cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,8 +4,21 @@ import KoaStatic from 'koa-static';
 import renderRouter from './routers/render/render';
 import apiRouter from './routers/api/api';
 import serverConf from '~/server';
+import http from '~/http';
 
 const { staticRoot } = serverConf;
+
+app.use(async (ctx, next) => {
+    // TODO 对于复杂请求的首次OPTION请求，必须返回一个OK的结果，后续的请求才可以继续发送。
+    if (ctx.method === 'OPTIONS') {
+        ctx.res.statusCode = 200;
+        ctx.set(http.cors);
+    }
+
+    // TODO将中间件的结果返回才会接着执行
+    await next()
+})
+
 app.use(renderRouter);
 app.use(apiRouter);
 
