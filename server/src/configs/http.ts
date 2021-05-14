28553bad99d1fa2193acdb4394eb3abf1d9e89a8
askwuxue <972523419@qq.com>
2021-05-15 00:01:34 +0800

    add http request relative configuration file

---
+++ server/src/configs/http.ts
@@ -0,0 +1,10 @@
+export default {
+    cors: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'X-Custom-Header, Content-Type'
+    },
+    simple: {
+        'Access-Control-Allow-Origin': '*'
+    }
+}
