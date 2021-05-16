4c39e3477674705afba53bbdd889fca9344a8d62
askwuxue <972523419@qq.com>
2021-05-16 23:22:54 +0800

    not use alias and craco

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import { getAllBanners, BannerData } from '@/models/banners';
+import { getAllBanners, BannerData } from './models/banners';
 import "./App.css";
 
 function App() {
