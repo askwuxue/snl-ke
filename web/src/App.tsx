2c20c7cd0a2996567720ece2a4797c71f01e7c42
askwuxue <972523419@qq.com>
2021-05-20 23:32:45 +0800

    import Login component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,30 @@
-import React, {useState, useEffect} from "react";
-import { getAllBanners, BannerData } from './models/banners';
+// import React, {useState, useEffect} from "react";
+// import { getAllBanners, BannerData } from './models/banners';
+import Login from './components/login';
 import "./App.css";
 
 function App() {
 
-  const [data, setData] = useState<BannerData[]>([]);
-  useEffect(() => {
-    (async () => {
-      let res = await getAllBanners();
-      setData(res);
-    })()
-  }, [])
+  return <Login></Login>
+
+  // const [data, setData] = useState<BannerData[]>([]);
+  // useEffect(() => {
+  //   (async () => {
+  //     let res = await getAllBanners();
+  //     setData(res);
+  //   })()
+  // }, [])
 
  
-  return <div className="App">
-    aaa
-    {
-      data.map(item => {
-        return <p key={item.ID}>{item.href}</p>
-      })
-    }
-  </div>;
+  // return <div className="App">
+  //   aaa
+  //   {
+  //     data.map(item => {
+  //       return <p key={item.ID}>{item.href}</p>
+  //     })
+  //   }
+  // </div>;
+
 }
 
 export default App;
