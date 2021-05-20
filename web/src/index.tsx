0f0f7270e12fb36887977fd3baa1296e65bf948b
askwuxue <972523419@qq.com>
2021-05-20 23:33:28 +0800

    import index.less and base.less

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './assets/css/index.less';
+import './assets/css/base.less';
 
 ReactDOM.render(
   <React.StrictMode>
