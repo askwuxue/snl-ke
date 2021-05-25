9e60308109a566d7c9acea147086b41cd972e6e6
askwuxue <972523419@qq.com>
2021-05-25 23:53:07 +0800

    add KEY_APP_CATEGORY_CATCH and erport

--- server/src/libs/redis.ts
+++ server/src/libs/redis.ts
@@ -7,4 +7,6 @@ const client = redis.createClient(redisConf);
 // TODO promisify将回调类型的函数转换为promise风格，会导致this丢失。需要重新绑定this
 export const set = promisify(client.set).bind(client);
 export const get = promisify(client.get).bind(client);
+export const KEY_APP_CATEGORY_CATCH = 'KEY_APP_CATEGORY_CATCH'; 
+
 
