74c73ee1b036c163cb252599fe9886a44b026048
askwuxue <972523419@qq.com>
2021-05-25 23:56:55 +0800

    solve lint style

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
+// import React, {useState, useEffect} from "react";
 // import { getAllBanners, BannerData } from './models/banners';
 // import Login from './components/login';
 import Header from './components/header/header';
