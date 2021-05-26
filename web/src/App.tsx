4c7fbd2609a861aa64b5a2c98b4543fb1850423c
askwuxue <972523419@qq.com>
2021-05-26 23:23:22 +0800

    add Header component and pass props

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,29 +4,14 @@ import React from "react";
 // import Login from './components/login';
 import Header from './components/header/header';
 import "./App.css";
+import CategoryData from "./models/category";
 
-function App() {
+interface Props {
+  categories: CategoryData[]
+}
 
-  // return <Login></Login>
-  return <Header />
-
-  // const [data, setData] = useState<BannerData[]>([]);
-  // useEffect(() => {
-  //   (async () => {
-  //     let res = await getAllBanners();
-  //     setData(res);
-  //   })()
-  // }, [])
-
- 
-  // return <div className="App">
-  //   aaa
-  //   {
-  //     data.map(item => {
-  //       return <p key={item.ID}>{item.href}</p>
-  //     })
-  //   }
-  // </div>;
+function App(props: Props) {
+  return <Header categories={props.categories}/>
 
 }
 
