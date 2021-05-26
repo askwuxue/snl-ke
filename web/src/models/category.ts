bd72ff87e0e9ce54ec2c923cc4af3430feb9b6b4
askwuxue <972523419@qq.com>
2021-05-26 23:27:12 +0800

    front edge not data time, issue a request

--- web/src/models/category.ts
+++ web/src/models/category.ts
@@ -1,6 +1,16 @@
+import axios from 'axios';
 export default interface CategoryData {
     ID: Number,
     title: String,
-    item?: CategoryData,
-    children?: CategoryData
+    items?: CategoryData[],
+    children?: CategoryData[]
+}
+
+// 请求数据后台数据
+export async function getCategories(): Promise<CategoryData[]> {
+    let { data } = await axios({
+        method: 'get',
+        url: 'http://localhost:8000/api/site/categories'
+    });
+    return data;
 }
