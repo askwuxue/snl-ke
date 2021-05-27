e2a605e5ca466365b40d5709dae95cb12ff4dde6
askwuxue <972523419@qq.com>
2021-05-27 22:21:15 +0800

    App component to add appData prop

--- server/src/routers/render/render.tsx
+++ server/src/routers/render/render.tsx
@@ -13,11 +13,14 @@ const mode = process.env.NODE_ENV;
 
 router.get('/', async ctx => {
     let categories = await getCategory();
+    let appData = {
+        categories
+    }
     // TODO 生产环境开启SSR
     if (mode === 'production') {
         const indexHtml = fs.readFileSync(`${staticRoot}/index.html`).toString();
         // ReactDOMServer.renderToString(<App />) 将组建转成了普通的html,js代码
-        const app = ReactDOMServer.renderToString(<App categories={categories}/>);
+        const app = ReactDOMServer.renderToString(<App appData={appData}/>);
         // TODO 实际上服务端只是在后端根据模板渲染数据，样式文件，图片等文件。都是前端打包好的
         // 后端配置webpack对样式等资源的处理因为引入了一个前端的组件，这个组件中使用了资源。
         const res = indexHtml.replace('<div id="root"></div>',
