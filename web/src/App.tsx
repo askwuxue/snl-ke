bbf6c913478093626e9ffcd210a8c557b70fe4d4
askwuxue <972523419@qq.com>
2021-05-27 22:19:46 +0800

    accept props and set data to models

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,18 +1,20 @@
 import React from "react";
-// import React, {useState, useEffect} from "react";
-// import { getAllBanners, BannerData } from './models/banners';
-// import Login from './components/login';
 import Header from './components/header/header';
 import "./App.css";
-import CategoryData from "./models/category";
+import { AppData, setAppData } from "./models/app";
 
 interface Props {
-  categories: CategoryData[]
+  appData?: AppData
 }
 
+// 如果接受到了数据，就保存到model的数据中。这个地方如果有数据，是后端渲染数据
 function App(props: Props) {
-  return <Header categories={props.categories}/>
-
+  // console.log('SSR render App props: ', JSON.stringify(props.appData));
+  // 如果接收到props，设置到models中。前端项目开始的组件是index。index不会传递props到当前组件
+  props.appData && setAppData(props.appData)
+  return (
+     <Header/>
+  )
 }
 
 export default App;
