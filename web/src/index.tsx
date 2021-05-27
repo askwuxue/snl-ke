1c7a999807febb5369fb7f2d3913105676782cf4
askwuxue <972523419@qq.com>
2021-05-27 22:21:46 +0800

    delete comments

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -8,14 +8,12 @@ import './assets/css/base.less';
 import './assets/css/common.less';
 
 // TODO 后端渲染，为Windows对象添加categories属性数据。前端加载时，直接可以获取数据
-const categories = (window as any).categories;
-// console.log(process.env.NODE_ENV);
 // TODO 根据当前的环境决定是否使用hydrate渲染
 const mode = process.env.NODE_ENV;
 const render = mode === 'development' ? ReactDOM.render : ReactDOM.hydrate;
 render(
   <React.StrictMode>
-    <App categories={categories}/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
