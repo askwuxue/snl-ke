6d99b0caeeeda174f53372e7de6bb80ce4730a11
askwuxue <972523419@qq.com>
2021-05-27 22:24:03 +0800

    resolve bug is categories show after search and accept data function

--- web/src/components/header/category.tsx
+++ web/src/components/header/category.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import CategoryData from '../../models/category';
+import React, { useState, useEffect } from 'react';
+import CategoryData, { getCategories } from '../../models/category';
+import { appData } from '../../models/app';
+
 interface Props {
-    categories: CategoryData[]
 }
 export default function Category(props: Props) {
+    // 数据的来源可能是models前端开始渲染之后存在models中的,或者是window对象中,也就是后端渲染来的数据
+    const [categories, setCategories] = useState<CategoryData[]>(appData.categories || (window as any | undefined).categories);
+    
+    useEffect(() => {
+        // TODO如果不存在props.categories，请求接口取数据
+        if (!categories) {
+            // models得到的是以Promise类型的数据
+            getCategories().then(data => {
+                setCategories(data);
+            })
+        }
+    }, [categories]) 
     return (
         <div className="category">
             <div className="title-container">
@@ -12,68 +25,71 @@ export default function Category(props: Props) {
             </div>
             <div className="list-container">
             <ul className="list">
+                {/* TODO 如果当前的categories是undefined，上面的骨架正常渲染，下面的内容不渲染 */}
                 {
-                    props.categories.map(category => {
-                        return (
-                            <li key={category.ID.toString()}>
-                            <div className="content">
-                                <div className="title">{category.title}</div>
-                                <div className="sub-title">
+                    categories ? (
+                        categories.map(category => {
+                            return (
+                                <li key={category.ID.toString()}>
+                                <div className="content">
+                                    <div className="title">{category.title}</div>
+                                    <div className="sub-title">
+                                        {
+                                            category.items ? (
+                                                <ul>
+                                                    {
+                                                        category.items.map(item => {
+                                                            return (
+                                                                <li key={item.ID.toString()}>{item.title}</li>
+                                                            )
+                                                        })
+                                                    }
+                                                </ul>
+                                            ) : ''
+                                        }
+                                    <ul>
+                                    </ul>
+                                    </div>
+                                </div>
+                                <div className="sub-list">
+                                    
                                     {
-                                        category.items ? (
-                                            <ul>
+                                        category.children ? (
+                                            <ul className="sub-list-ul">
                                                 {
-                                                    category.items.map(item => {
+                                                    category.children.map(children => {
                                                         return (
-                                                            <li key={item.ID.toString()}>{item.title}</li>
+                                                            <li className="sub-list-li" key={children.ID.toString()}>
+                                                                <div className="main">
+                                                                {children.title}
+                                                                </div>
+                                                                <div className="list">
+                                                                    {
+                                                                        children.items ? (
+                                                                            <ul>
+                                                                                {
+                                                                                    children.items.map(item => {
+                                                                                        return (
+                                                                                                <li key={item.ID.toString()}>{item.title}</li>
+                                                                                        )
+                                                                                    })
+                                                                                }
+                                                                            </ul>
+                                                                        ) : ''
+                                                                    }
+                                                                </div>
+                                                            </li>
                                                         )
-                                                    })
+                                                    })                                         
                                                 }
                                             </ul>
                                         ) : ''
                                     }
-                                <ul>
-                                </ul>
                                 </div>
-                            </div>
-                            <div className="sub-list">
-                                
-                                {
-                                    category.children ? (
-                                        <ul className="sub-list-ul">
-                                            {
-                                                category.children.map(children => {
-                                                    return (
-                                                        <li className="sub-list-li" key={children.ID.toString()}>
-                                                            <div className="main">
-                                                            {children.title}
-                                                            </div>
-                                                            <div className="list">
-                                                                {
-                                                                    children.items ? (
-                                                                        <ul>
-                                                                            {
-                                                                                children.items.map(item => {
-                                                                                    return (
-                                                                                            <li key={item.ID.toString()}>{item.title}</li>
-                                                                                    )
-                                                                                })
-                                                                            }
-                                                                        </ul>
-                                                                    ) : ''
-                                                                }
-                                                            </div>
-                                                        </li>
-                                                    )
-                                                })                                         
-                                            }
-                                        </ul>
-                                    ) : ''
-                                }
-                            </div>
-                        </li>    
-                        )
-                    })
+                            </li>    
+                            )
+                        })
+                    ) : <></>
                 }
             </ul>
             </div>
