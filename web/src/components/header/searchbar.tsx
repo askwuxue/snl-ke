7301ed2409017118f19dfee80645d66958379503
askwuxue <972523419@qq.com>
2021-05-31 23:45:48 +0800

    get data failed time pull request and get data

--- web/src/components/header/searchbar.tsx
+++ web/src/components/header/searchbar.tsx
@@ -1,5 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { appData } from '../../models/app';
+import { SearchData, getSearchData } from '../../models/search';
+
 export default function SearchBar() {
+    const [ searchData, setSearchData ] = useState<SearchData | undefined>(appData?.searchData);
+    useEffect(() => {
+        if (!searchData) {
+            getSearchData().then(data => {
+                // console.log(data.result);
+                setSearchData(data);
+            })
+        }
+    }, [searchData])
+
     return (
         <div className="searchbar">
         <div className="search">
@@ -12,11 +25,19 @@ export default function SearchBar() {
         <div className="list">
         <div className="title">热门搜索</div>
         <ul>
-            <li>ps</li>
+            {/* <li>ps</li>
             <li>高等数学</li>
             <li>吉他</li>
             <li>mybatis</li>
-            <li>英语四级</li>
+            <li>英语四级</li> */}
+            {
+                searchData ? (
+                    searchData.result.map((data, index) => 
+                        <li key={index}>{data}</li> 
+                    )
+                ) : ''
+            }
+
         </ul>
         </div>
     </div>
