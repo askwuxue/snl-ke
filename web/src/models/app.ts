7b324c8e26ccac725732a5272ee0792cf328b756
askwuxue <972523419@qq.com>
2021-05-31 22:28:29 +0800

    edit appData

--- web/src/models/app.ts
+++ web/src/models/app.ts
@@ -1,8 +1,10 @@
 import CategoryData from '../models/category';
+import { SearchData } from '../models/search';
 
 // 定义数据格式
 export interface AppData {
-    categories: CategoryData[] | undefined
+    categories: CategoryData[] | undefined;
+    searchData: SearchData | undefined
 }
 
 // 对数据进行修改保存
@@ -10,7 +12,14 @@ export function setAppData(data: AppData) {
     appData = data;
 }
 
-// 定义导出的数据格式
-export let appData: AppData = {
-    categories: undefined
+// 定义导出的数据格式,如果浏览器环境，才使用window上的属性，不是浏览器环境默认为undefined
+export let appData: AppData;
+if (typeof window !== undefined) {
+    appData = (window as any).appData;
+    console.log('appData: ', appData);
+} else {
+    appData = {
+        categories: undefined,
+        searchData: undefined
+    }
 }
