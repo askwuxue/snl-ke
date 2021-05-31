e4fc68c405ea704ca148e0cb8f0c3871c30cec1d
askwuxue <972523419@qq.com>
2021-05-31 23:46:25 +0800

    pull request

--- web/src/models/search.ts
+++ web/src/models/search.ts
@@ -1,4 +1,12 @@
-// import db from '../../../server/src/libs/database';
+import axios from 'axios';
 export interface SearchData {
     result: String[];
+}
+
+export async function getSearchData(): Promise<SearchData> {
+    let { data } = await axios({
+        method: 'get',
+        url: 'http://localhost:8000/api/site/get-searchdata'
+    })
+    return data;
 }
