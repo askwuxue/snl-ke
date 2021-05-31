7f9d74ffdaa47e82a2231bf9a7bda30d6e779d29
askwuxue <972523419@qq.com>
2021-05-31 23:44:03 +0800

    add get-searchdata api

--- server/src/routers/api/site/site.ts
+++ server/src/routers/api/site/site.ts
@@ -1,6 +1,7 @@
 import Router from '@koa/router';
 import { getCategory } from '../../../models/category';
 import http from '~/http';
+import { getSuggestion } from '../../../models/search';
 
 const router = new Router();
 router.prefix('/site');
@@ -8,6 +9,11 @@ router.prefix('/site');
 router.get('/categories', async ctx => {
     ctx.set(http.simple);
     ctx.body = await getCategory();
+})
+
+router.get('/get-searchdata', async ctx => {
+    ctx.set(http.cors);
+    ctx.body = await getSuggestion('');
 })
 
 // 将路由的routes导出作为一个中间件使用
