1e09f9f98a55f2bd2b085291811fe31ecc3cd7a7
askwuxue <972523419@qq.com>
2021-06-02 00:33:35 +0800

    update database query

--- server/src/models/banner.ts
+++ server/src/models/banner.ts
@@ -4,8 +4,9 @@ import { BannerData } from '@/models/banners';
 // 返回的是Promise类型的BannerData数组
 export async function getAllBanners(): Promise<BannerData[]> {
     // TODO mysql查询到的数组中，数据是rows, files是连接相关的信息。
-    const result = await database.query('SELECT * from banner_table;');
-    // TODO 将result 强制转成BannerData类型的数组
-    const rows = result[0] as BannerData[];
-    return rows;
+    const result = (await database.query(`SELECT ID, src, href 
+        FROM banner 
+        ORDER BY sort 
+        ASC;`))[0] as BannerData[];
+    return result;
 }
