bf3bf358cbde601c2e958924200bd67277b28781
askwuxue <972523419@qq.com>
2021-06-02 21:43:20 +0800

    update query statement and bgColor field

--- server/src/models/banner.ts
+++ server/src/models/banner.ts
@@ -4,7 +4,7 @@ import { BannerData } from '@/models/banners';
 // 返回的是Promise类型的BannerData数组
 export async function getAllBanners(): Promise<BannerData[]> {
     // TODO mysql查询到的数组中，数据是rows, files是连接相关的信息。
-    const result = (await database.query(`SELECT ID, src, href 
+    const result = (await database.query(`SELECT ID, src, href, bgColor  
         FROM banner 
         ORDER BY sort 
         ASC;`))[0] as BannerData[];
